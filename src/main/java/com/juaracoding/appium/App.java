2fef6f5ba6f2c2b209b2bcaa76e128c95e8eda3e
Yohanes Adeo <82396456+yohanesadeo@users.noreply.github.com>
2025-10-06 17:22:42 +0700

    done

---
+++ src/main/java/com/juaracoding/appium/App.java
@@ -0,0 +1,13 @@
+package com.juaracoding.appium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
